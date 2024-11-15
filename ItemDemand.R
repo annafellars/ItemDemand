8ba830e5e90674f33f325a42a36b329809ad27f7
Anna Fellars <anna@fellars.com>
2024-11-15 10:06:24 -0700

    full eda

--- ItemDemand.R
+++ ItemDemand.R
@@ -2,6 +2,7 @@ library(vroom)
 library(forecast)
 library(dplyr)
 library(ggplot2)
+library(patchwork)
 
 train_data <- vroom("./train.csv")
 test_data <- vroom("./test.csv")
@@ -40,3 +41,5 @@ plot5 <- store2 |>
 plot6 <- store2 |>
   pull(sales) |>
   forecast::ggAcf(lag.max = 2*365)
+
+((plot1 | plot2 | plot3) / (plot4 | plot5 | plot6))
